792b979df469846cfabadbba3939dd6e07e6598f
Emilia Kalayuki <kalayuki.emilia@gmail.com>
2022-07-28 16:14:47 -0300

    primer commit

---
+++ js/clase1.js
@@ -0,0 +1,77 @@
+/*let num1= 12;
+let num2= 21;
+alert(num1+num2)
+console.log(num1+num2)
+let suma= num1+num2;
+console.log("la suma es un conjunto de numeros:"+suma)
+console.log("coder"+"house")
+
+let nombredepersona = prompt ("ingresar nombre"); 
+console.log (nombredepersona)
+
+let nume1=parseInt(prompt("1er numero"))
+let nume2=parseInt(prompt("2do numero"))
+
+console.log (nume1 + nume2)*/
+
+//clase 2
+/*
+let edad="emilia";
+
+if (edad >18){
+ alert ("puede ingresar")
+} else if (edad < 18 ){
+    alert ("es menor de 18, no puede ingresar")
+}else if (edad == 18){
+    alert ("tiene 18, puede ingresar")
+}else {
+    alert ("no ingreso su edad")
+}
+*/
+/*
+let nombreIngresado = prompt ("ingresar nombre");
+let apellidoIngresar= prompt ("ingresar apellido");
+if ((nombreIngresado !="")&&(apellidoIngresar !="")){
+    alert ("nombre: " + nombreIngresado + "\nApellido "+ apellidoIngresar)
+}else{
+    alert ("erro:no ingreso nombre y apellido")
+}*/
+/*
+//ejemplo formulario de turnos.
+for(let i=1; 1<=20;i++){
+    let ingreseNombre=prompt("Ingrese su nombre");
+    alert ("Su turno es el N°: "+i+" Nombre de pacientes: "+ ingreseNombre);
+}
+*/
+/*
+//Sentencia de Break
+for (let i=1; i<=20; i++){
+    let ingresarNombre = prompt ("Ingrese su nombre");
+    if (i==10){
+        break
+    }
+    alert ("turno n°:"+i+"Nombre:"+ ingresarNombre)
+}
+*/
+
+let dato1=prompt("ingresar dato")
+//let dato2=prompt("ingre datos2")
+//let dato3=prompt("ingrese dato 3")
+
+if (isNaN (dato1)){
+    alert ("el dato e sun texto")
+} else{
+   alert ("el dato es un numero")
+}
+/*
+if (isNaN (dato2)){
+    alert ("el dato es un texto")
+}else {
+    alert ("el dato es un numero")
+}
+if (isNaN (dato3)){
+    alert ("el dato es un texto")
+}else {
+    alert (" El dato es un numero")
+}
+*/
